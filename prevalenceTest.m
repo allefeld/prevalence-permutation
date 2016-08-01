b59e6c2f23c00688ec504ee1ba89771d39d902c8
Carsten Allefeld <carsten.allefeld@bccn-berlin.de>
2016-08-01 22:23:56 +0200

    adapt prevalenceTest.m to external data set

--- prevalenceTest.m
+++ prevalenceTest.m
@@ -1,20 +1,23 @@
 % test the implementation of 'permutation-based prevalence inference
 % using the minimum statistic'
 %
-% This script uses the data contained in the subdirectory
-% cichy-2011-category-smoothedaccuracy to test the present implementation
-% of prevalence inference, and to check whether changes of the code change
-% the result.
+% This script uses the data set 'cichy-2011-category-smoothedaccuracy' to
+% test the present implementation of prevalence inference, and to check
+% whether changes of the code change the result. The data are not included
+% with the implementation, but can be downloaded from
+%   https://github.com/allefeld/cichy-2011-category-smoothedaccuracy/releases/tag/v1.0.0
+% Unzip the archive within this directory, so that its contents are in the
+% subdirectory cichy-2011-category-smoothedaccuracy-1.0.0.
 %
-% It can also be taken as an example for how to use the implementation.
+% This script can also be taken as an example for how to use the implementation.
 %
-% With parameter P2 changed to 1e7, this script should reproduce the
-% results shown in Allefeld, Goergen & Haynes (2016), Fig 4a and b, except
-% for minor differences due to Monte Carlo estimation.
+% With parameter P2 changed to 1e7, it should reproduce the results shown
+% in Allefeld, Goergen & Haynes (2016), Fig 4a and b, except for minor
+% differences due to Monte Carlo estimation. 
 %
 % WARNING: The script resets the random number generator to a fixed seed in
 % order to obtain reproducible results. Make sure to reinitialize it again
-% to a time-based seed aftewards: rng('shuffle')
+% to a time-based seed afterwards: rng('shuffle')
 %
 % On 2016-7-31 with Matlab 8.5.0 (R2015a) and SPM8 r4290 under Linux 3.16,
 % this script generated image files with the following md5 checksums:
@@ -28,14 +31,20 @@
 
 clear, close all
 
+% check if data is present
+datadir = 'cichy-2011-category-smoothedaccuracy-1.0.0';
+if ~exist(datadir, 'dir')
+    fprintf('please download test data, see `help prevalenceTest`\n')
+    return
+end
+
 % collect input image filenames
 N = 12;
-P1 = 4 ; 16;
-inputPat = 'cichy-2011-category-smoothedaccuracy/%02d/sa_C0002_P%04d.nii.gz';
+P1 = 16;
 input = cell(N, P1);
 for k = 1 : N
     for i = 1 : P1
-        input{k, i} = sprintf(inputPat, k, i);
+        input{k, i} = sprintf([datadir '/%02d/sa_C0002_P%04d.nii.gz'], k, i);
     end
 end
 
@@ -49,7 +58,7 @@ mkdir(tn);
 rng(42)
 
 % perform prevalence inference
-P2 = 200; %000;
+P2 = 200000;
 alpha = 0.05;
 prefix = [tn '/prevalence_'];
 prevalence(input, P2, alpha, prefix)
